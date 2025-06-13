01a7f05154fcf9b1c3579679a86b4d0b69ff66de
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-06-13 07:34:14 -0600

    Documentacion de apis

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,19 @@ import Login from "./src/Routes/Login.js";
 import RecoveryRoutes from "./src/Routes/RecoveryRoutes.js"
 import Logout from "./src/Routes/LogoutRoutes.js";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./scp001Documentacion.json"), "utf-8")
+  );
 
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/PeliRoutes",PeliRoutes);
 app.use("/api/empleadoRoutes",empRoutes);
 app.use("/api/registerEmpRoutes",regiempRoutes);
